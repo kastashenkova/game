0c3e2ff464a788eba5ff16df496f3c2901379bc4
kastashenkova <astashenkova.katya@gmail.com>
2025-05-28 10:38:36 +0300

    try: add heroe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,42 @@
 package org.example;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
 
 public class Main extends SimpleApplication {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Main app = new Main();
-        app.start(); // start the game
+        app.start();
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1); // create cube shape
-        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
-        Material mat = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
-        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
-        geom.setMaterial(mat);                   // set the cube's material
-        rootNode.attachChild(geom);              // make the cube appear in the scene
+        // Підключення локальної папки з моделлю
+        assetManager.registerLocator("assets", FileLocator.class);
+
+        // Завантаження героя
+        Spatial hero = assetManager.loadModel("Models/Hero/heroe.gltf");
+
+        // Додати героя на сцену
+        rootNode.attachChild(hero);
+
+        hero.scale(0.05f); // або 0.1f, залежно від ситуації
+
+
+        cam.setLocation(new Vector3f(0, 2, 10)); // Позиція камери
+        cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y); // Дивимось на центр
+
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-1, -2, -3).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
+
+
+
     }
 }
